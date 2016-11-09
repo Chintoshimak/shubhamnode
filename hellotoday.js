46313c5c8200e06e5b14f272a21c2710e0339d8f
Shubham Saxena <meincredible846@gmail.com>
2016-11-09 06:33:40 -0600

    efjskfsebf

--- hellotoday.js
+++ hellotoday.js
@@ -1,13 +1,14 @@
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 8080);
 var http = require('http');
-var server = http.createServer( function(request, response) {
-var body = 'Hello WebSphere!';
-response.writeHead( 200, {
-'Content-Length': body.length,
-'Content-Type': 'text/plain'
-});
-response.write(body);
-response.end();
+var server = http.createServer(function(request, response) {
+    var body = 'Hello WebSphere!';
+    response.writeHead(200, {
+        'Content-Length': body.length,
+        'Content-Type': 'text/plain'
+    });
+    response.write(body);
+    response.end();
 });
 server.listen(port);
+//a
